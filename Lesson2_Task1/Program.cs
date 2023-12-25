dafa26d32f0f845c43b75c6ffe571f3c1c2e09d5
Sudev Nikolay <SudevNN@mail.ru>
2023-12-26 00:00:04 +0500

    add Lesson2_Task1

---
+++ Lesson2_Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// "&&" = "И": апельсины И яблоки (покупаем и то, и другое)
+// "||" = "ИЛИ": апельсины ИЛИ яблоки(что-то одно, либо все вместе)
+if (number >= 100 && number <= 999)
+{
+    int firstDigit = number / 100; // 275 / 100 = 2
+    int thirdDigit = number % 10; // 275 % 10 = 5
+    int result = firstDigit * 10 + thirdDigit;// 2 * 10+ 5 = 25
+    //$"Текст {firstDigit} текст {firstDigit * 10 + thirdDigit}"
+    Console.WriteLine($"Результат: {number} => {result}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное ИЛИ отрц.");
+}
